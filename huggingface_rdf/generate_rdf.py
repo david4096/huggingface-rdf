219e01cbe37a18a1cc3cfacb91899c84ac2119fc
Edwin Ong Jun Kiat <164170699+Edwin-Ong-Jun-Kiat@users.noreply.github.com>
2024-11-07 12:03:31 +0000

    Update generate_rdf.py

--- huggingface_rdf/generate_rdf.py
+++ huggingface_rdf/generate_rdf.py
@@ -11,7 +11,7 @@ def chunk_data(data, chunk_size):
     for i in range(0, len(data), chunk_size):
         yield data[i:i + chunk_size]
             
-def convert_to_rdf(data, output_file,base="http://fakebase",chunk_size=10):
+def convert_to_rdf(data, output_file,base="http://fakebase"):
     """
     This function takes a JSON-serializable data structure, converts it to RDF using 
     JSON-LD format, and serializes it into Turtle format, saving it to the specified file.
@@ -26,6 +26,7 @@ def convert_to_rdf(data, output_file,base="http://fakebase",chunk_size=10):
         str: A string representation of the RDF graph in Turtle format.
     """
     total_items = len(data)
+    chunk_size = total_items // 100 
     logging.info(f"Starting RDF conversion. Total items: {total_items}, Chunk size: {chunk_size}")
 
     g = Graph()
@@ -40,3 +41,4 @@ def convert_to_rdf(data, output_file,base="http://fakebase",chunk_size=10):
     logging.info(f"RDF data successfully saved to {output_file}")
     # Implementation for generating RDF
     return g.serialize(destination=output_file, format='ttl')
+    
